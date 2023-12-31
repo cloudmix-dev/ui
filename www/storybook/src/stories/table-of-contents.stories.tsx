cbe379195178d9c57c190640f82a0aabd1b31ccf
Sam Laycock <samuel.paul.laycock@gmail.com>
2023-12-31 18:15:50 +0000

    fix: fix scroll highlight logic for TableOfContents component

--- www/storybook/src/stories/table-of-contents.stories.tsx
+++ www/storybook/src/stories/table-of-contents.stories.tsx
@@ -23,32 +23,38 @@ type TableOfContentsStory = StoryObj<typeof TableOfContents>;
 export const Default: TableOfContentsStory = {
   render: () => (
     <div className="flex w-full">
-      <div className="flex-grow">
+      <div id="scroll" className="flex-grow h-48 overflow-scroll">
         <article>
           <Prose>
             <section>
-              <h2>Heading 1</h2>
+              <h1 id="heading-1">Heading 1</h1>
               <p>Some content underneath heading 1</p>
             </section>
             <hr />
             <section>
-              <h2>Heading 2</h2>
+              <h2 id="heading-2">Heading 2</h2>
               <p>Some content underneath heading 2</p>
-              <h3>Heading 3</h3>
+              <h3 id="heading-3">Heading 3</h3>
               <p>Some content underneath heading 3</p>
             </section>
             <hr />
             <section>
-              <h2>Heading 4</h2>
+              <h2 id="heading-4">Heading 4</h2>
               <p>Some content underneath heading 4</p>
-              <h3>Heading 5</h3>
+              <h3 id="heading-5">Heading 5</h3>
               <p>Some content underneath heading 5</p>
             </section>
           </Prose>
         </article>
       </div>
-      <div className="flex-shrink-0 w-48">
-        <TableOfContents />
+      <div className="relative flex-shrink-0 w-48">
+        <div className="sticky top-0">
+          <TableOfContents
+            contentQuerySelector="article"
+            scrollQuerySelector="#scroll"
+            scrollOffset={64}
+          />
+        </div>
       </div>
     </div>
   ),
